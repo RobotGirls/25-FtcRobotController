e0c3618c148f99538b6ac0cf22fbcfdd4783d7df
katelynl19 <katelynliu@rocknrollrobots25.com>
2024-10-14 16:25:50 -0700

    updated roadrunner tuning files and autonomous and installed MeepMeep

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ITDTeleopRoadrunner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ITDTeleopRoadrunner.java
@@ -1,14 +1,9 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
-import com.acmerobotics.roadrunner.Pose2d;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.Range;
-
-import org.firstinspires.ftc.teamcode.RNRRMecanumDrive;
 
 @TeleOp(name = "Teleop Drive Only")
 public class ITDTeleopRoadrunner extends LinearOpMode {
