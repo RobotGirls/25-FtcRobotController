c1797af9a493b492896ddb1c718f9d824a028625
Anneke Wolpert <annekewolpert@rocknrollrobots25.com>
2024-10-24 18:12:52 -0700

    created/edited a bran new auto for the red left fot the 2024-25 season

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/RedLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/RedLeft.java
@@ -1,43 +1,47 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
 // RR-specific imports
+
 import androidx.annotation.NonNull;
 
-//import com.acmerobotics.dashboard.config.Config;
-
-// Non-RR imports
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
-import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.teamcode.drive.RNRRMecanumDrive;
 
 //@Config
 @Autonomous(name = "TEST_AUTO_Anneke")
-public class TestAuto extends LinearOpMode {
+public class RedLeft extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         // instantiating the robot at a specific pose
-        Pose2d initialPose = new Pose2d(0, 0, Math.toRadians(90));
+        Pose2d initialPose = new Pose2d(-38, -62, Math.toRadians(89));
         RNRRMecanumDrive drive = new RNRRMecanumDrive(hardwareMap, initialPose);
 
         Lift lift = new Lift(hardwareMap);
         Claw claw = new Claw(hardwareMap);
+        LiftPivot liftPivot = new LiftPivot(hardwareMap);
 
         // actionBuilder builds from the drive steps passed to it
         TrajectoryActionBuilder toBasket = drive.actionBuilder(initialPose)
-                .lineToYSplineHeading(33, Math.toRadians(0));
+                .lineToY(-1)
+                .waitSeconds(2)
+                .turn(Math.toRadians(-96.5))
+                .waitSeconds(1)
+                .lineToX(-23)
+
+
 //                .waitSeconds(2)
 //                .setTangent(Math.toRadians(90))
 //                .lineToY(48)
@@ -46,8 +50,7 @@ public class TestAuto extends LinearOpMode {
 //                .strafeTo(new Vector2d(44.5, 30))
 //                .turn(Math.toRadians(180))
 //                .lineToX(47.5)
-//                .waitSeconds(3);
-
+                 .waitSeconds(3);
 
         // ON INIT:
         Actions.runBlocking(claw.closeClaw());
@@ -66,9 +69,10 @@ public class TestAuto extends LinearOpMode {
         Actions.runBlocking(
                 new SequentialAction(
                         firstTraj, // go to the basket
-                        lift.liftUp(), // to high basket
-                        claw.openClaw(), // drop the sample
-                        lift.liftDown()
+                        liftPivot.liftPivotUp()
+                        //lift.liftUp() // to lvl1 ascent
+                      //  claw.openClaw(), // drop the sample
+                      //  lift.liftDown()
                 )
         );
     }
@@ -97,7 +101,7 @@ public class TestAuto extends LinearOpMode {
                 // checks lift's current position
                 double pos = lift.getCurrentPosition();
                 packet.put("liftPos", pos);
-                if (pos < 3000.0) {
+                if (pos < 500.0) {
                     // true causes the action to rerun
                     return true;
                 } else {
@@ -172,5 +176,75 @@ public class TestAuto extends LinearOpMode {
             return new CloseClaw();
         }
     }
+
+    public class LiftPivot {
+        private DcMotorEx liftPivot;
+
+        public LiftPivot(HardwareMap hardwareMap) {
+            liftPivot = hardwareMap.get(DcMotorEx.class, "liftPivot");
+            liftPivot.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            liftPivot.setDirection(DcMotorSimple.Direction.FORWARD);
+        }
+
+        public class LiftPivotUp implements Action {
+            // checks if the lift motor has been powered on
+            private boolean initialized = false;
+
+            // actions are formatted via telemetry packets as below
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                // powers on motor, if it is not on
+                if (!initialized) {
+                    liftPivot.setPower(0.8);
+                    initialized = true;
+                }
+                // checks lift's current position
+                double pos = liftPivot.getCurrentPosition();
+                packet.put("liftPivotPos", pos);
+                if (pos < 500.0) {
+                    // true causes the action to rerun
+                    return true;
+                } else {
+                    // false stops action rerun
+                    liftPivot.setPower(0);
+                    return false;
+                }
+                // overall, the action powers the lift until it surpasses
+                // 3000 encoder ticks, then powers it off
+            }
+        }
+        public Action liftPivotUp() {
+            return new LiftPivotUp();
+        }
+/*
+        public class LiftDown implements Action {
+            private boolean initialized = false;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+                    lift.setPower(-0.8);
+                    initialized = true;
+                }
+
+                double pos = lift.getCurrentPosition();
+                packet.put("liftPos", pos);
+                if (pos > 100.0) {
+                    return true;
+                } else {
+                    lift.setPower(0);
+                    return false;
+                }
+            }
+        }
+
+        public Action liftDown() {
+            return new LiftDown();
+        }
+
+ */
+
+    }
+
 }
 
