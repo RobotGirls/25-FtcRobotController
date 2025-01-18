b813600cef242e50878c55c648458ca7f4c80749
Anneke Wolpert <annekewolpert@rocknrollrobots25.com>
2025-01-18 14:01:29 -0800

    seperated this spline code from MeepMeep into individual actions to allow mechanisms to run

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/RedLeftBasketThenParkNEWPATH.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/RedLeftBasketThenParkNEWPATH.java
@@ -39,28 +39,48 @@ public class RedLeftBasketThenParkNEWPATH extends LinearOpMode {
 
         // actionBuilder builds from the drive steps passed to it
         TrajectoryActionBuilder toBasket = drive.actionBuilder(initialPose)
-                .lineToY(-33.5)
-                .turn(Math.toRadians(-90))
-                .lineToX(34)
-                .splineTo(new Vector2d(34,-4),Math.toRadians(0))
-                .turn(Math.toRadians(-90))
-                //  .strafeTo(new Vector2d(34,-4))
-                .strafeTo(new Vector2d(48,-4))
-                .strafeTo(new Vector2d(48,-58))
-                .strafeTo(new Vector2d(48,-4))
-                .strafeTo(new Vector2d(56,-4))
-                .strafeTo(new Vector2d(56,-58))
-                .strafeTo(new Vector2d(56,-4))
-                .strafeTo(new Vector2d(61,-4))
-                .strafeTo(new Vector2d(61,-58));
+                .lineToY(-52)
+                .turn(Math.toRadians(90))
+                .lineToX(-52)
+                .turn(Math.toRadians(45));
 
+        TrajectoryActionBuilder sampleOne = toBasket.endTrajectory().fresh()
+                .turn(Math.toRadians(45))
+                //         .splineTo(new Vector2d(-35,-52),180) too wavy .
+                .strafeTo(new Vector2d(-35,-52))
+                .strafeTo(new Vector2d(-35,-10))
+                .strafeTo(new Vector2d(-46,-10));
 
+        TrajectoryActionBuilder toBasketAgain = sampleOne.endTrajectory().fresh()
+                .splineTo(new Vector2d(-52,-52),Math.toRadians(-90))
+                .turn(Math.toRadians(-45));
+
+        TrajectoryActionBuilder sampleTwo = toBasketAgain.endTrajectory().fresh()
+                .lineToX(-48)
+                .splineTo(new Vector2d(-35,-10),Math.toRadians(0))
+                //   .lineToY(-10)
+                .lineToX(-54)
+                .turn(Math.toRadians(90));
+
+        TrajectoryActionBuilder finalToBasket = sampleTwo.endTrajectory().fresh()
+                .splineTo(new Vector2d(-52,-52),Math.toRadians(-90))
+                .turn(Math.toRadians(-45));
+
+        Action backToSub = finalToBasket.endTrajectory().fresh()
+                .turn(Math.toRadians(90))
+                .splineTo(new Vector2d(-24,-5),Math.toRadians(0))
+                .build();
 
 
         // ON INIT:
-  //      Actions.runBlocking(claw.closeClaw());
+  //      Actions.runBlocking(claw.closeClaw())
 
         Action firstTraj = toBasket.build();
+        Action secondTraj = sampleOne.build();
+        Action thirdTraj = toBasketAgain.build();
+        Action fourthTraj = sampleTwo.build();
+        Action fifthTraj = finalToBasket.build();
+
 
         while (!isStopRequested() && !opModeIsActive()) {
             telemetry.addData("Robot position: ", drive.updatePoseEstimate());
@@ -73,18 +93,36 @@ public class RedLeftBasketThenParkNEWPATH extends LinearOpMode {
         // running the action sequence!
         Actions.runBlocking(
                 new SequentialAction(
-//                        liftPivot.liftPivotDown(),
-                        firstTraj // go to the basket, push samples, and then submersible
-                        /*
+                        liftPivot.liftPivotDown(),
+                        firstTraj,  // get to basket to drop preload
                         liftPivot.liftPivotUp(),
                         lift.liftUp(),
                         claw.openClaw(), // drop the sample
                         lift.liftDown(),
-                         // push samples, go to submersible
+                        liftPivot.liftPivotDown(),
+                        secondTraj, // get to the first sample to intake
+                        lift.liftUpLittle(),
+                        claw.closeClaw(), // lift out lift a little and intake the sample
+                        lift.liftDown(),
+                        thirdTraj, // get back to the basket to drop other sample
                         liftPivot.liftPivotUp(),
-                        lift.liftUp()
+                        lift.liftUp(),
+                        claw.openClaw(), // drop the sample
+                        lift.liftDown(),
+                        liftPivot.liftPivotDown(),
+                        fourthTraj, // get to the next sample to intake
+                        lift.liftUpLittle(),
+                        claw.closeClaw(), // lift out lift a little and intake the sample
+                        lift.liftDown(),
+                        fifthTraj, // go to the basket one final time to prepare to drop the sample
+                        liftPivot.liftPivotUp(),
+                        lift.liftUp(),
+                        claw.openClaw(), // drop the sample
+                        lift.liftDown(),
+                        liftPivot.liftPivotDown(),
+                        backToSub, // head back to the submersible to park
+                        liftPivot.liftPivotUp() // level 1 ascent
 
-                         */
                 )
         );
     }
