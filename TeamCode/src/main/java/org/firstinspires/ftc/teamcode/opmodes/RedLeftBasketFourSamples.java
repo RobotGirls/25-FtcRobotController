1becdd6c6cde2ff1878ff195e84ede089e56bc5d
Sydney Ettenger <sydneyettenger@rocknrollrobots25.com>
2025-02-22 16:07:48 -0800

    added new auto pathway called RedLeftBasketFourSamples

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/RedLeftBasketFourSamples.java
@@ -0,0 +1,83 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+// RR-specific imports
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+//@Config
+@Autonomous(name = "RedRightTripleSpeciman")
+public class RedRightTripleSpeciman extends LinearOpMode {
+    private boolean first = true;
+    private static final double FIRST_LIFT_DOWN_POS = 50.0;
+    private static final double LAST_LIFT_DOWN_POS = 100.0;
+    private double currLiftPos = 0.0;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // instantiating the robot at a specific pose
+        Pose2d initialPose = new Pose2d(8, -60, Math.toRadians(90));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+
+     //   Lift lift = new Lift(hardwareMap);
+     //   Claw claw = new Claw(hardwareMap);
+        LiftPivot liftPivot = new LiftPivot(hardwareMap);
+
+        // actionBuilder builds from the drive steps passed to it
+        TrajectoryActionBuilder toChamber = drive.actionBuilder(initialPose)
+                .lineToY(-33.5)
+                .turn(Math.toRadians(-90))
+                .lineToX(34)
+                .splineTo(new Vector2d(34,-4),Math.toRadians(0))
+                .turn(Math.toRadians(-90))
+                //  .strafeTo(new Vector2d(34,-4))
+                .strafeTo(new Vector2d(48,-4))
+                .strafeTo(new Vector2d(48,-58))
+                .strafeTo(new Vector2d(48,-4))
+                .strafeTo(new Vector2d(56,-4))
+                .strafeTo(new Vector2d(56,-58))
+                .strafeTo(new Vector2d(56,-4))
+                .strafeTo(new Vector2d(61,-4))
+                .strafeTo(new Vector2d(61,-58));
+
+        // ON INIT:
+  //      Actions.runBlocking(claw.closeClaw());
+
+        Action firstTraj = toChamber.build();
+
+        while (!isStopRequested() && !opModeIsActive()) {
+            telemetry.addData("Robot position: ", drive.updatePoseEstimate());
+            telemetry.update();
+        }
+        waitForStart();
+        if (isStopRequested()) return;
+
+        // IN RUNTIME
+        // running the action sequence!
+        Actions.runBlocking(
+                new SequentialAction(
+//                        liftPivot.liftPivotDown(),
+                        firstTraj // go to the chamber, push sample, park in observation zone
+                        //lift.liftUp() // to lvl1 ascent
+                      //  claw.openClaw(), // drop the sample
+                      //  lift.liftDown()
+                )
+        );
+    }
+
+}
