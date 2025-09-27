f82e5bcb16490b7aeb7a0948dfb99e63043f03e5
Esther Lin <elin@Esthers-MacBook-Pro.local>
2025-09-27 13:13:44 -0700

    made the color sensor its own class and made a test opmode. uncommented everything

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/ColorSensorHSVTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/ColorSensorHSVTest.java
@@ -1,28 +1,28 @@
-//package org.firstinspires.ftc.teamcode.test;
-//
-//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-//import org.firstinspires.ftc.teamcode.test.HSVColorSensor;
-//
-//@TeleOp(name = "Color Sensor HSV Test A")
-//public class ColorSensorHSVTest extends LinearOpMode {
-//
-//    HSVColorSensor colorSensor = new HSVColorSensor();
-//    private HSVColorSensor.DetectedColor artifactColor = HSVColorSensor.DetectedColor.UNKNOWN;
-//
-//    @Override
-//    public void runOpMode() throws InterruptedException {
-//        colorSensor.initColorSensor(hardwareMap);
-//        while (!isStarted()) {
-//        colorSensor.getDetectedColor(telemetry);
-//        artifactColor = colorSensor.getCurrentDetectedColor();
-//        telemetry.addData("Artifact Color",artifactColor);
-//        }
-//        // wait for PLAY button to be pushed
-//        waitForStart();
-//        while (opModeIsActive()) {
-//            colorSensor.getDetectedColor(telemetry);
-//        }
-//    } // end runOpMode
-//}
-//
+package org.firstinspires.ftc.teamcode.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.test.HSVColorSensor;
+
+@TeleOp(name = "Color Sensor HSV Test A")
+public class ColorSensorHSVTest extends LinearOpMode {
+
+    HSVColorSensor colorSensor = new HSVColorSensor();
+    private HSVColorSensor.DetectedColor artifactColor = HSVColorSensor.DetectedColor.UNKNOWN;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        colorSensor.initColorSensor(hardwareMap);
+        while (!isStarted()) {
+        colorSensor.getDetectedColor(telemetry);
+        artifactColor = colorSensor.getCurrentDetectedColor();
+        telemetry.addData("Artifact Color",artifactColor);
+        }
+        // wait for PLAY button to be pushed
+        waitForStart();
+        while (opModeIsActive()) {
+            colorSensor.getDetectedColor(telemetry);
+        }
+    } // end runOpMode
+}
+
