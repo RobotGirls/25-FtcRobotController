915051319224aa6385b46d10614d3f0d494de904
Anneke Wolpert <annekewolpert@rocknrollrobots25.com>
2025-10-06 20:18:17 -0700

    created a MeepMeep folder and a MeepMeep testing file to create an auto for the red side. Unfortunately there is no field for the DECODE season yet so we used the simple blue-tile backgrounds and referenced a photo of the game field to estimate where playing objects are located.

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,34 +1,42 @@
 package com.example.meepmeeptesting;
 
-import com.acmerobotics.roadrunner.geometry.Pose2d;
-
-import org.rowlandhall.meepmeep.MeepMeep;
-import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
-import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
-            RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
-                    // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                    .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                    .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                            .forward(30)
-                            .turn(Math.toRadians(90))
-                            .forward(30)
-                            .turn(Math.toRadians(90))
-                            .forward(30)
-                            .turn(Math.toRadians(90))
-                            .forward(30)
-                            .turn(Math.toRadians(90))
-                            .build());
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
 
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(56, -53, Math.toRadians(315)))
+                .lineToX(0)
+                .waitSeconds(.5)
+                .turn(Math.toRadians(-45))
+                .strafeTo(new Vector2d(8, -31))
+                .lineToY(-24)
+                .turn(Math.toRadians(45))
+                .strafeTo(new Vector2d(40, -24))
+                .build());
 
-            meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
-                    .setDarkMode(true)
-                    .setBackgroundAlpha(0.95f)
-                    .addEntity(myBot)
-                    .start();
-        }
+//                .turn(Math.toRadians(90))
+//                .lineToY(30)
+//                .turn(Math.toRadians(90))
+//                .lineToX(0)
+//                .turn(Math.toRadians(90))
+//                .lineToY(0)
+//                .turn(Math.toRadians(90))
+
+        meepMeep.setBackground(MeepMeep.Background.GRID_BLUE)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
     }
+}
