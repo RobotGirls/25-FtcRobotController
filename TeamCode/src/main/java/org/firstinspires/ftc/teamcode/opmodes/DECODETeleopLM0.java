e24a6e72a07195d912ad6cc1ea368cb748e36dc1
katelynl19 <katelynliu@rocknrollrobots25.com>
2025-10-18 16:14:20 -0400

    updated teleop to include shooter, transfer, and intake

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/DECODETeleopLM0.java
@@ -0,0 +1,106 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+
+@TeleOp(name = "Teleop LM3 BUTTONS")
+public class DECODETeleopLM0 extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    public DcMotor leftFront   = null;
+    public DcMotor  rightFront  = null;
+    public DcMotor  rightBack  = null;
+    public DcMotor  leftBack  = null;
+
+    public DcMotor shooter;
+    public DcMotor transfer;
+    public DcMotor intake;
+
+
+    @Override
+    public void runOpMode() {
+
+        // Define and Initialize Motors
+        leftFront = hardwareMap.get(DcMotor.class, "frontLeft");
+        rightFront = hardwareMap.get(DcMotor.class, "frontRight");
+        rightBack = hardwareMap.get(DcMotor.class, "backRight");
+        leftBack = hardwareMap.get(DcMotor.class, "backLeft");
+
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        //RNRRMecanumDrive drive = new RNRRMecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+        //drive.setPoseEstimate(startPose);
+
+        shooter = hardwareMap.get(DcMotor.class, "shooter");
+        transfer = hardwareMap.get(DcMotor.class, "transfer");
+        intake = hardwareMap.get(DcMotor.class, "intake");
+
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
+        // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
+        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
+
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData(">", "Robot Ready.  Press START.");    //
+        telemetry.update();
+
+        // Wait for the game to start (driver presses START)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            leftFront.setPower(0.8*frontLeftPower);
+            leftBack.setPower(0.8*backLeftPower);
+            rightFront.setPower(0.8*frontRightPower);
+            rightBack.setPower(0.8*backRightPower);
+
+            if (gamepad2.left_bumper) {
+                intake.setPower(1);
+                transfer.setPower(1);
+            }
+            else if (gamepad2.right_bumper) {
+                intake.setPower(-1);
+                transfer.setPower(-1);
+            }
+            else {
+                intake.setPower(0);
+                transfer.setPower(0);
+            }
+            if (gamepad2.x) {
+                shooter.setPower(-1);
+            }
+            else {
+                shooter.setPower(0);
+            }
+           
+
+            // Pace this loop so jaw action is reasonable speed.
+            sleep(50);
+        }
+    }
+}
