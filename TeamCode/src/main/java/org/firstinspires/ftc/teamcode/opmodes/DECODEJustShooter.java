573d642688d91e870869367ac765106f8fdf967b
katelynl19 <katelynliu@rocknrollrobots25.com>
2025-12-08 20:01:41 -0800

    changed test code to just one motor for shooter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/DECODEJustShooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/DECODEJustShooter.java
@@ -26,9 +26,9 @@ public class DECODEJustShooter extends LinearOpMode {
     public void runOpMode() {
 
         shooter1 = hardwareMap.get(DcMotor.class, "shooter1");
-        shooter2 = hardwareMap.get(DcMotor.class,"shooter2");
+        //shooter2 = hardwareMap.get(DcMotor.class,"shooter2");
         //shooter1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        shooter2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        shooter1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
         // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
@@ -46,17 +46,17 @@ public class DECODEJustShooter extends LinearOpMode {
 
             if (gamepad2.x) {
                 shooter1.setPower(shooterSpeed);
-                shooter2.setPower(-1*shooterSpeed);
+                //shooter2.setPower(-1*shooterSpeed);
                 flywheelState = FlywheelState.ON;
             }
            else if (gamepad2.b) {
                 shooter1.setPower(-1*shooterSpeed);
-                shooter2.setPower(shooterSpeed);
+                //shooter2.setPower(shooterSpeed);
                 flywheelState = FlywheelState.ON;
             }
             else {
                 shooter1.setPower(0);
-                shooter2.setPower(0);
+                //shooter2.setPower(0);
             }
 
             // Pace this loop so jaw action is reasonable speed.
