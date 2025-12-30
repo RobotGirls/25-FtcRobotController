ab9513c8e6ae10dc3bf0b1c45e6870c541b7d593
Esther Lin <elin@Esthers-MacBook-Pro.local>
2025-12-29 20:21:54 -0800

    tested limelight on our new robot and got the robot pose

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/SixWheelLimelightTesting.java
@@ -0,0 +1,145 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLStatus;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+
+@TeleOp(name = "SixWheelLimelightTesting")
+
+public class SixWheelLimelightTesting extends LinearOpMode {
+
+    private Limelight3A limelight;
+    private DcMotor turret;
+    private final int ALIGN_THRESHOLD = 3;
+    private double lastError = 0;
+    private double derivative;
+    private double integralSum = 0;
+
+    private double Kp = 0.014; // Tx range is 0 to 26 --> at max offset 26, when Kp is 0.02, speed is half power
+    private double Ki = 0;
+    private double Kd = 0;
+    private double shooterSpeed = 0.75;
+
+    public DcMotor  leftFront   = null;
+    public DcMotor  rightFront  = null;
+    public DcMotor  rightBack  = null;
+    public DcMotor  leftBack  = null;
+
+    public final double TURRET_OFFSET = 90; // FIXME figure out how many degrees to the side the turret will be aiming relative to front of robot
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        leftFront = hardwareMap.get(DcMotor.class, "frontLeft");
+        rightFront = hardwareMap.get(DcMotor.class, "frontRight");
+        rightBack = hardwareMap.get(DcMotor.class, "backRight");
+        leftBack = hardwareMap.get(DcMotor.class, "backLeft");
+
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        turret = hardwareMap.get(DcMotor.class, "turret");
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+
+        telemetry.setMsTransmissionInterval(11);
+
+        limelight.pipelineSwitch(2);
+
+        /*
+         * Starts polling for data.  If you neglect to call start(), getLatestResult() will return null.
+         */
+        limelight.start();
+
+        telemetry.addData(">", "Robot Ready.  Press Play.");
+        telemetry.update();
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            leftFront.setPower(frontLeftPower);
+            leftBack.setPower(backLeftPower);
+            rightFront.setPower(frontRightPower);
+            rightBack.setPower(backRightPower);
+
+            LLStatus status = limelight.getStatus();
+
+            LLResult result = limelight.getLatestResult();
+
+            if (result.isValid()) {
+
+                // Access general information
+                double captureLatency = result.getCaptureLatency();
+                double targetingLatency = result.getTargetingLatency();
+                double parseLatency = result.getParseLatency();
+
+                telemetry.addData("tx", result.getTx());
+                telemetry.addData("txnc", result.getTxNC());
+                telemetry.addData("ty", result.getTy());
+                telemetry.addData("tync", result.getTyNC());
+                Pose3D botpose = result.getBotpose();
+                double robotx = botpose.getPosition().x;
+                double roboty = botpose.getPosition().y;
+                telemetry.addData("MT1 Location", "(" + robotx + ", " + roboty + ")");
+                if (robotx < -0.5) {
+                    shooterSpeed = 0.5;
+                } else if (robotx >=-0.5 && robotx <0.5) {
+                    shooterSpeed = 0.6;
+                } else {
+                    shooterSpeed = 0.75;
+                }
+                telemetry.addData("shooterSpeed", shooterSpeed);
+                telemetry.update();
+
+
+                double error = result.getTx();
+                ElapsedTime timer = new ElapsedTime();
+                if (Math.abs(error) > ALIGN_THRESHOLD) {
+                    error = -1 * result.getTx();
+                    derivative = (error - lastError) / timer.seconds();
+                    integralSum = integralSum + (error * timer.seconds());
+                    double power = (Kp * error) + (Ki * integralSum) + (Kd * derivative);
+                    turret.setPower(power);
+                    lastError = error;
+                } else {
+                    turret.setPower(0);  // aligned
+                }
+
+
+            } else {
+                // if we don't see an apriltag
+                turret.setPower(0);
+                telemetry.addData("Limelight", "No data available");
+
+                //turret.setTargetPosition(robot.getHeading() - TURRET_OFFSET);
+            }
+
+            telemetry.update();
+        }
+        limelight.stop();
+    }
+}
