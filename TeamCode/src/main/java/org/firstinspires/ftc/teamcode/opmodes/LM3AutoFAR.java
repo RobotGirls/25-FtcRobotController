ac3f8a324cddb91ef17a85f0c99dd38c532c9ddb
katelynl19 <katelynliu@rocknrollrobots25.com>
2026-01-17 14:17:00 -0800

    LM3 changes: added far auto, added close autos that only shoot preloads, added hood to teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/LM3AutoFAR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/LM3AutoFAR.java
@@ -7,20 +7,20 @@ import com.acmerobotics.roadrunner.ParallelAction;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
-import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.TankDrive;
-
 import org.firstinspires.ftc.teamcode.mechanismCode.IntakeRoadRunner;
 import org.firstinspires.ftc.teamcode.mechanismCode.ShooterRoadRunner;
 import org.firstinspires.ftc.teamcode.mechanismCode.TransferRoadRunner;
 
 //@Config
-@Autonomous(name = "LM3 Red")
-public class LM3AutoRed extends LinearOpMode {
+@Autonomous(name = "LM3 FAR")
+public class LM3AutoFAR extends LinearOpMode {
+
+    public final double FLYWHEEL_SPEED_LONG = -0.8;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -28,35 +28,20 @@ public class LM3AutoRed extends LinearOpMode {
         // telemetry.setAutoClear(false);
         // liftTimer.reset();
         // instantiating the robot at a specific pose
-        Pose2d initialPose = new Pose2d(-52, 46, Math.toRadians(130));
+        Pose2d initialPose = new Pose2d(55, -16, Math.toRadians(180));
         TankDrive drive = new TankDrive(hardwareMap, initialPose);
-        IntakeRoadRunner intake= new IntakeRoadRunner(hardwareMap,telemetry);
         ShooterRoadRunner shooter = new ShooterRoadRunner(hardwareMap, telemetry);
-        TransferRoadRunner transfer = new TransferRoadRunner(hardwareMap,telemetry);
+        IntakeRoadRunner intake = new IntakeRoadRunner(hardwareMap,telemetry);
+        TransferRoadRunner transfer = new TransferRoadRunner(hardwareMap, telemetry);
 
         // actionBuilder builds from the drive steps passed to it
 
         TrajectoryActionBuilder toShoot = drive.actionBuilder(initialPose)
-                .lineToY(8);
+                .lineToX(40);
 
-        TrajectoryActionBuilder intakeBalls = toShoot.endTrajectory().fresh()
-                .turn(Math.toRadians(-130))
-                .lineToX(-12)
-                .waitSeconds(0.1)
-                .turn(Math.toRadians(90))
-                .lineToY(48);
-        TrajectoryActionBuilder backToShoot = intakeBalls.endTrajectory().fresh()
-                .lineToY(8);
-        Action outOfZone = backToShoot.endTrajectory().fresh()
-                .turn(Math.toRadians(90))
-                .lineToX(16)
-                .build();
 
 
         Action firstTraj = toShoot.build();
-        Action secondTraj = intakeBalls.build();
-        Action thirdTraj = backToShoot.build();
-
 
         while (!isStopRequested() && !opModeIsActive()) {
             telemetry.addData("Robot position: ", drive.updatePoseEstimate());
@@ -69,37 +54,22 @@ public class LM3AutoRed extends LinearOpMode {
         // running the action sequence!
         Actions.runBlocking(
                 new SequentialAction(
-                        firstTraj,
+
                         shooter.shootArtifact(),
                         new ParallelAction(
                                 shooter.shootArtifact(),
                                 intake.intakeArtifact(),
                                 transfer.intakeArtifact()
                         ),
+                        firstTraj
 
-                        new ParallelAction(
-                              secondTraj,
-                                intake.intakeArtifact(),
-                                transfer.intakeArtifact()
-                        ),
-
-                        new ParallelAction(
-                        thirdTraj,
-                        shooter.shootArtifact()
-                                ),
-                        new ParallelAction(
-                                shooter.shootArtifact(),
-                                transfer.intakeArtifact(),
-                                intake.intakeArtifact()
-                        ),
-                        outOfZone
 
                 )
         );
-
     }
 
-}
 
+
+}
 
 
