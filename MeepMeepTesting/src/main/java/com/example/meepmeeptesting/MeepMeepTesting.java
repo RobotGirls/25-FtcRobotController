fa359853df3da03335da0adb7105dd35a3fb4e0b
Esther Lin <eyx326@gmail.com>
2026-09-19 13:03:53 -0700

    added a backup auto path and a new class for it

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -21,11 +21,19 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-70, 38, Math.toRadians(0)))
-                .strafeToLinearHeading(new Vector2d(-50, 15), 0)
-                .waitSeconds(1)
-                .setTangent(Math.toRadians(90))
-                .splineToLinearHeading(new Pose2d(new Vector2d(30, 60), 0), Math.toRadians(0))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(56, 15, Math.toRadians(-150)))
+                .splineToLinearHeading(new Pose2d(50, 15, Math.toRadians(180)), Math.toRadians(180))
+                .waitSeconds(2)
+                .splineToLinearHeading(new Pose2d(60, 15, Math.toRadians(0)), Math.toRadians(-100))
+                .waitSeconds(2)
+                .splineToLinearHeading(new Pose2d(58, 15, Math.toRadians(180)), Math.toRadians(180))
+                .waitSeconds(2)
+                .splineToLinearHeading(new Pose2d(30, 60, Math.toRadians(180)), Math.toRadians(180))
+
+//                .strafeToLinearHeading(new Vector2d(-50, 15), 0)
+//                .waitSeconds(1)
+//                .setTangent(Math.toRadians(90))
+//                .splineToLinearHeading(new Pose2d(new Vector2d(30, 60), 0), Math.toRadians(0))
 
 //
                 .build());
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/paths/LM0BlueFlower.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode.opmodes.auto.paths;
+
+// RR-specific imports
+
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+//@Config
+@Autonomous(name = "LM0 Blue Flower", group = "Blue Auto")
+public class LM0BlueFlower extends LinearOpMode {
+    private boolean first = true;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // liftTimer.reset();
+        // instantiating the robot at a specific pose
+        Pose2d initialPose = new Pose2d(56, 15, Math.toRadians(-150));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+
+
+        // actionBuilder builds from the drive steps passed to it
+        TrajectoryActionBuilder toHive = drive.actionBuilder(initialPose)
+                .strafeToLinearHeading(new Vector2d(-50, 15), 0);
+
+        Action toPark = toHive.endTrajectory().fresh()
+                .setTangent(Math.toRadians(90))
+                .splineToLinearHeading(new Pose2d(new Vector2d(30, 60), 0), Math.toRadians(0))
+
+                .build();
+
+        Action firstTraj = toHive.build();
+
+        while (!isStopRequested() && !opModeIsActive()) {
+            telemetry.addData("Robot position: ", drive.updatePoseEstimate());
+            telemetry.update();
+        }
+        waitForStart();
+        if (isStopRequested()) return;
+
+        // IN RUNTIME
+        // running the action sequence!
+        Actions.runBlocking(
+                new SequentialAction(
+                        firstTraj,
+                        toPark
+                )
+        );
+
+        // add mechanism code below
+
+    }
+
+}
